84e28c7303d94065d1c3735d39ca6b6ed1002f2e
mrdarip <darioelas2005@gmail.com>
2024-08-02 12:03:13 +0200

    implemented iterating over all characters

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var currentChar = 0;
+
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-const a = 0.0082550335570469798657718120805369127516778523489932885906040268;
-const b = 0.0453305785123966942148760330578512396694214876033057851239669421;
-const xOff = 9.84;
-const yOff = 9.89;
 
-ctx.fillStyle = "grey";
-ctx.fillRect(0, 0, 500, 500);
+drawCanvas();
 
-//write letter "a"
-ctx.fillStyle = "black";
-ctx.font = "bold 600px Comic Sans MS";
-ctx.fillText("H",50,430);
+canvas.addEventListener('mousedown', function(e) {
+    getCursorPosition(canvas, e)
+    
+    currentChar++;
+    drawCanvas();
+})
+
+function drawCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0, 0, 500, 500);
+    
+    ctx.fillStyle = "black";
+    ctx.font = "bold 600px Comic Sans MS";
+    ctx.fillText(characters[currentChar],50,430);
+    
+}
 
 function getCursorPosition(canvas, event) {
     const rect = canvas.getBoundingClientRect()
@@ -19,18 +31,3 @@ function getCursorPosition(canvas, event) {
     const y = ((-(event.clientY - rect.top) + 430) /384) * 35.45
     console.log(x+", " + y)
 }
-
-canvas.addEventListener('mousedown', function(e) {
-    getCursorPosition(canvas, e)
-})
-
-/*
-With consolas font
-
-should be
-2.58 0          27.97 0
-
-
-is
-77  -430            351  -430
-*/
