534ede5973f266a5dd2cb218642f8a4b302a549b
mrdarip <darioelas2005@gmail.com>
2024-08-02 10:26:34 +0200

    implemented getting click position

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ ctx.fillRect(0, 0, 500, 500);
 ctx.fillStyle = "black";
 ctx.font = "bold 500px Comic Sans MS";
 ctx.fillText("Hello World",0,450);
+
+function getCursorPosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    console.log("x: " + x + " y: " + y)
+}
+
+canvas.addEventListener('mousedown', function(e) {
+    getCursorPosition(canvas, e)
+})
