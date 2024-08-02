c154f89320162c7061f4dc4d63d69dfffdb2061b
mrdarip <darioelas2005@gmail.com>
2024-08-02 10:59:36 +0200

    added values to js code, its not working

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+const a = 0.0082550335570469798657718120805369127516778523489932885906040268;
+const b = 0.0453305785123966942148760330578512396694214876033057851239669421;
+const xOff = 9.84;
+const yOff = 9.89;
 
 ctx.fillStyle = "grey";
 ctx.fillRect(0, 0, 500, 500);
@@ -11,9 +15,9 @@ ctx.fillText("Hello World",50,430);
 
 function getCursorPosition(canvas, event) {
     const rect = canvas.getBoundingClientRect()
-    const x = event.clientX - rect.left
-    const y = event.clientY - rect.top
-    console.log("x: " + x + " y: " + y)
+    const x = (event.clientX - rect.left) * a + xOff
+    const y = (event.clientY - rect.top) * b + yOff
+    console.log(x+", " + y)
 }
 
 canvas.addEventListener('mousedown', function(e) {
