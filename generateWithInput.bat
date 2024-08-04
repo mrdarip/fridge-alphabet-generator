70938d7c8a82cdb8a31be7a2b95b2a68fdccdee6
mrdarip <darioelas2005@gmail.com>
2024-08-04 22:07:04 +0200

    measured a real magnet and found out that 10 is a pretty big magnet size, reduced it to 8 and the height to 5mm, going from 5000 stl's to 2000, reduzing it by 60%

--- generateWithInput.bat
+++ generateWithInput.bat
@@ -48,9 +48,9 @@ echo %positions% >> charsAndPos.scad
 echo ; >> charsAndPos.scad
 
 REM Square magnets
-FOR /L %%H IN (1,1,10) DO (
-    FOR /L %%D IN (1,1,10) DO (
-        echo %%D/10 mm diameter, %%H/10 mm height
+FOR /L %%H IN (1,1,8) DO (
+    FOR /L %%D IN (1,1,8) DO (
+        echo %%D/8 mm diameter, %%H/8 mm height
         mkdir "magnets\square\%%Dmm_diameter\%%Hmm_height"
         FOR /L %%A IN (0,1,25) DO (
             call echo %%chars%%A%%
@@ -60,8 +60,8 @@ FOR /L %%H IN (1,1,10) DO (
 )
 
 REM Round magnets
-FOR /L %%H IN (1,1,10) DO (
-    FOR /L %%D IN (1,1,10) DO (
+FOR /L %%H IN (1,1,8) DO (
+    FOR /L %%D IN (1,1,8) DO (
         echo %%D mm diameter, %%H mm height
         mkdir "magnets\round\%%Dmm_diameter\%%Hmm_height"
         FOR /L %%A IN (0,1,25) DO (
